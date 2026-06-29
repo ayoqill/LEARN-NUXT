a87fe435a4bb4be83432c6557de44bade9ecc963
ayoqill <aqilaiman058@gmail.com>
2026-06-29 15:59:21 +0800

    v-model is for user input data

---
+++ Vue_Recap/v-model.vue
@@ -0,0 +1,23 @@
+
+<!-- use v-model to connect an input field with a variable 
+  -- use for input fields -->
+  
+
+  <script setup>
+    import {ref} from 'vue'
+
+    const message = ref('Hello Vue World')
+  </script>
+
+  <template>
+    input field: <input v-model="message" type="text"/> <!-- HERE: the is the user type input -->
+    <p>message: {{ message }}</p>
+  </template>
+
+
+/*
+    Must Understand:
+        When user types in input, the Vue data changes.
+        When Vue data changes, the input display changes too.
+*/
+
