f8fb3d8e231bc1dca260431b2467b7a07d1988d2
ayoqill <aqilaiman058@gmail.com>
2026-06-29 14:36:25 +0800

    ref() reactive variable

---
+++ Vue_Recap/ref()_and_variables.vue
@@ -0,0 +1,43 @@
+
+<!-- ref means reactive variable -->
+
+<script setup>
+    import {ref} from 'vue'
+
+    const message = ref('Hello Vue World')
+
+    function changeMessage() {
+    message.value = 'Message changed!'
+    }
+</script>
+
+<template>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">Change Message</button>
+</template>
+
+/*
+    Must Understand:
+    1. ref is used to create a reactive variable in Vue.
+    2. The value of the reactive variable can be accessed using .value.
+    3. When the value of the reactive variable changes, the template will automatically update to reflect the new value.
+*/
+
+/*
+    Example:
+    const message = ref('Hello Vue World')
+    message.value = 'Hello Vue Universe' // This will update the template to display 'Hello Vue Universe'
+*/
+
+/*
+    Note:
+    1. ref is a function that takes an initial value and returns a reactive reference to that value.
+    2. The reactive reference can be used in the template to display the value and will automatically update when the value changes.
+*/
+
+/*
+    Simple memory:
+    ref() = make data reactive
+    {{ }} = show data on screen
+    .value = access/change ref value inside script
+*/
