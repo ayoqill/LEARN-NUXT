5d05edf6810d07511058e5c065b572a37dba6277
ayoqill <aqilaiman058@gmail.com>
2026-06-29 16:04:40 +0800

    use of array and v-for example

---
+++ Vue_Recap/array + v-for.vue
@@ -0,0 +1,31 @@
+/*
+
+Example use of array and v-for in Vue.js
+
+*/
+
+<script setup>
+import { ref } from 'vue'
+
+const messages = ref([
+  { id: 1, text: 'Hello', sender: 'user' },
+  { id: 2, text: 'Hi there', sender: 'bot' }
+])
+</script>
+
+<template>
+  <div v-for="msg in messages" :key="msg.id">
+    {{ msg.sender }}: {{ msg.text }}
+  </div>
+</template>
+
+
+/*
+    Summary:
+        messages = list of chat messages
+        msg = one message from the list
+        v-for = repeat the div for every message
+        msg.sender = who sent it
+        msg.text = the message
+        :key = unique identity for each item
+*/
