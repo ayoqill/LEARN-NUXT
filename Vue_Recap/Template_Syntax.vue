5921f7ca5f96fdc9518d393833375b1c15b7e08d
ayoqill <aqilaiman058@gmail.com>
2026-06-29 14:13:44 +0800

    basic template

--- Vue_Recap/Template_Syntax.vue
+++ Vue_Recap/Template_Syntax.vue
@@ -8,6 +8,40 @@
     const title = 'Hello Vue World'
 </script>
 
+----------------------------------------------
+<p v-if="typewritersInStock">
+  in stock
+</p>
+
+<p v-else>
+  not in stock
+</p>
+
+----------------------------------------------
+<p v-for="(x, index) in manyFoods">
+      {{ index }}: "{{ x.name }}", url: "{{ x.url }}" <br>
+</p>
+
+----------------------------------------------
+<div v-for="(img, index) in images" :key="index">
+  <img
+    :src="img.url"
+    @click="select(index)"
+    :class="{ selClass: img.sel }"
+  >
+</div>
+
+/*
+    :class="{ selClass: img.sel }"
+
+    This means:
+        Add the CSS class selClass only when img.sel is true.
+        Think of it like an if condition.
+*/
+
+
+-----------------------------------------------------------------------------------------------
+
 /*
     Must Understand:
     1. Vue uses double curly braces {{ }} to display data in the template.
