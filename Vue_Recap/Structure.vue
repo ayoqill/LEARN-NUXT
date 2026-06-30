b42ad5b6df219086039b71c66d059aca32148392
ayoqill <aqilaiman058@gmail.com>
2026-06-30 12:14:02 +0800

    structure, how to setup, pages and route

---
+++ Vue_Recap/Structure.vue
@@ -0,0 +1,15 @@
+/*
+    enygma-nuxt/
+    ├── app/
+    │   ├── app.vue                     this is the main app component
+    │   ├── assets/                     this is where you put your static assets like images, fonts, etc.    
+    │   ├── pages/                      this is where you put your web pages, each page is a vue component
+    │   ├── components/                 this is where you put your reusable UI parts
+    │   └── layouts/                    Use layout when many pages share the same design.
+    ├── public/                         this is where you put your static files that are served as-is, like favicon.ico
+    ├── server/                         this is where you put your server code, like API routes. Nuxt backend API routes. but we use Django for enygma.
+    ├── nuxt.config.ts                  this is the main configuration file for Nuxt / project settings
+    ├── package.json                    this is where you put your project dependencies and scripts
+    └── node_modules/                   this is where npm installs your project dependencies
+*/
+
---
+++ Vue_Recap/howtosetup.vue
@@ -0,0 +1,12 @@
+
+<!--
+
+    npm create nuxt@latest <project-name>
+    cd enygma-nuxt
+    npm install
+    npm run dev -- -o
+
+
+
+    npm run dev -- -o  to automatically open the browser when the dev server starts. 
+-->
